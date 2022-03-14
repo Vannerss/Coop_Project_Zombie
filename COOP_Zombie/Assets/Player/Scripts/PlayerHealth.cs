97b7ee9ff39ceb5eb8a637b1c774b37cfd0919ad
Alexios12345 <100698551+Alexios12345@users.noreply.github.com>
2022-03-14 18:46:18 -0400

    Created PlayerHealth

--- COOP_Zombie/Assets/Player/Scripts/PlayerHealth.cs
+++ COOP_Zombie/Assets/Player/Scripts/PlayerHealth.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float MaxHealth = 100f;
+    private float CurrentHealth;
+    private int Heal = 5;
+    private GameObject GameOver;
+    [SerializeField]
+    private Image HealthSlider;
+
+
+    private void Start()
     {
-        
+        CurrentHealth = MaxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+   
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Enemy") 
+        {
+            DamagePlayer(20);
+        }
+    }
+
+
+    private void DamagePlayer(int damage) 
+    {
+        CurrentHealth -= damage;
+       
+
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            HealthSlider.fillAmount = CurrentHealth;
+
+        }
+        else 
+        {
+            HealthSlider.fillAmount = CurrentHealth / MaxHealth;
+        }
+        Debug.Log(CurrentHealth);
+    }
+
+
+    private void HealPlayer() 
+    {
+        if (CurrentHealth > 100) 
+        {
+            CurrentHealth = MaxHealth;
+           
+        }
     }
 }
